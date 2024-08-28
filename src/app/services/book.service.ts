fb22fdff1873ac76722c30e62fdfc658beb886b8
Anette Villalón <liuflee@outlook.com>
2024-08-27 20:08:30 -0400

    a

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -5,9 +5,10 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
 export class BookService {
 
-  public apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=subject:fiction&orderBy=relevance&maxResults=10';
+  public apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=orderBy=relevance&maxResults=10';
 
   constructor(private http: HttpClient) {}
 
